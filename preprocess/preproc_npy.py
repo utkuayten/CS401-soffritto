5431bd8a78af198633e885c9f235ce444e9329c7
utkuayten <utkuayten7878@outlook.com>
2025-03-29 22:12:05 +0300

    add visualizations for predictions

--- preprocess/preproc_npy.py
+++ preprocess/preproc_npy.py
@@ -1,10 +1,9 @@
 import numpy as np
 
 # Replace 'results.npy' with the path to your file.
-pred = np.load('/Users/ozgun/DataspellProjects/CS401-soffritto/results/genomic_multitarget_informer/pred.npy')
-true = np.load('/Users/ozgun/DataspellProjects/CS401-soffritto/results/genomic_multitarget_informer/true.npy')
-metrics = np.load('/Users/ozgun/DataspellProjects/CS401-soffritto/results/genomic_multitarget_informer/metrics.npy')
-print(pred[0][-1],true[0][-1])
+pred = np.load('/Users/utkuayten/Desktop/CS401-soffritto/results/genomic_multitarget_informer/pred.npy')
+true = np.load('/Users/utkuayten/Desktop/CS401-soffritto/results/genomic_multitarget_informer/true.npy')
+
+print(pred[0],true[0])
 
 print(pred.shape,true.shape)
-print(metrics)
