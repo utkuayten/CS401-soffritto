39e0cf684149c94d6d6e0481c122ece62aa223c2
utkuayten <utkuayten7878@outlook.com>
2025-12-19 16:43:19 +0300

    modified:   transofritto_v2/informer/exp/exp_informer.py

--- transofritto_v2/informer/exp/exp_informer.py
+++ transofritto_v2/informer/exp/exp_informer.py
@@ -11,7 +11,7 @@ import warnings
 
 from transofritto.informer.data_loader.data_loader import Dataset_Custom
 from transofritto.informer.exp.exp_basic import Exp_Basic
-from transofritto.informer.models.model import Informer, InformerStack
+from transofritto_v2.informer.models.model import Informer, InformerStack
 from transofritto.informer.utils.metrics import metric
 from transofritto.informer.utils.tools import EarlyStopping, adjust_learning_rate
 
